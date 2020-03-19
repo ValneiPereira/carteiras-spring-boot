81fb221d311aadd714ca0c685e19b0366b876cc3
ValneiPereira <valnney@yahoo.com.br>
2020-03-18 21:38:52 -0300

    projeto criado

---
+++ src/main/java/com/carteira/CarteirasApplication.java
@@ -0,0 +1,13 @@
+package com.carteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteirasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carteira/CarteirasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
